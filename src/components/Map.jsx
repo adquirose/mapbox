b720323bd15bf734910e705c5d8e395067ad5d65
Arturo Quiros <adquirose@gmail.com>
2025-04-28 15:33:45 -0400

    fix1

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -191,7 +191,7 @@ export const Map = () => {
     }
   }, [lotes])
   return (
-    <div id="container-map">
+    <div style={{ position: 'relative', width: '100vw', height: '100%', minHeight:'100vh',backgroundColor: 'gray' }}>
       {loading && (
         <div
           style={{
