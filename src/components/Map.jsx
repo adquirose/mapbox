d3a0f507be29ee67227950f0b1a15d88a567e02b
Arturo Quiros <adquirose@gmail.com>
2025-04-28 21:42:02 -0400

    version 1.1

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import { collection, getDocs } from 'firebase/firestore';
-import { firebaseDB } from '../firebase/config';
-import CircularProgress from '@mui/material/CircularProgress';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { ArrowUpward, ArrowDownward, ArrowBack, ArrowForward, Add, Remove, RotateLeft, RotateRight } from '@mui/icons-material';
-import HomeIcon from '@mui/icons-material/Home';
-import { Box } from '@mui/material';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './Map.css';
+import React, { useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import { collection, getDocs } from "firebase/firestore";
+import { firebaseDB } from "../firebase/config";
+import CircularProgress from "@mui/material/CircularProgress";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import {
+  ArrowUpward,
+  ArrowDownward,
+  ArrowBack,
+  ArrowForward,
+  Add,
+  Remove,
+  RotateLeft,
+  RotateRight,
+} from "@mui/icons-material";
+import HomeIcon from "@mui/icons-material/Home";
+import { Box } from "@mui/material";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./Map.css";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+const calculateCentroid = (coordinates) => {
+  let area = 0;
+  let cx = 0;
+  let cy = 0;
+
+  const points = coordinates[0]; // Usamos el primer anillo del polígono
+  const numPoints = points.length;
+
+  for (let i = 0; i < numPoints - 1; i++) {
+    const [x1, y1] = points[i];
+    const [x2, y2] = points[i + 1];
+    const factor = x1 * y2 - x2 * y1;
+
+    area += factor;
+    cx += (x1 + x2) * factor;
+    cy += (y1 + y2) * factor;
+  }
+
+  area = area / 2;
+  cx = cx / (6 * area);
+  cy = cy / (6 * area);
+
+  return [cx, cy];
+};
 
 export const Map = () => {
   const [map, setMap] = useState(null);
@@ -25,7 +59,7 @@ export const Map = () => {
   useEffect(() => {
     const fetchLotes = async () => {
       try {
-        const lotesCollection = collection(firebaseDB, 'lotes');
+        const lotesCollection = collection(firebaseDB, "lotes");
         const lotesSnapshot = await getDocs(lotesCollection);
         const lotesData = lotesSnapshot.docs.map((doc) => {
           const data = doc.data();
@@ -33,7 +67,7 @@ export const Map = () => {
 
           if (data.geometry) {
             const geometry = JSON.parse(data.geometry);
-            if (geometry.type === 'Polygon' && geometry.coordinates) {
+            if (geometry.type === "Polygon" && geometry.coordinates) {
               coordinates = geometry.coordinates;
             }
           }
@@ -47,7 +81,7 @@ export const Map = () => {
 
         setLotes(lotesData);
       } catch (error) {
-        console.error('Error al obtener los lotes:', error);
+        console.error("Error al obtener los lotes:", error);
       }
     };
 
@@ -57,77 +91,83 @@ export const Map = () => {
   useEffect(() => {
     if (lotes.length > 0) {
       const mapInstance = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/adquirose/cma1kkbav003f01qn86l26jet',
+        container: "map",
+        style: "mapbox://styles/adquirose/cma1rg2im009y01qx64kj8k1j",
         center: [-72.2524, -45.3358],
         zoom: 10,
-        pitch: 60,
+        pitch: 0,
         bearing: 0,
       });
 
-      mapInstance.on('load', () => {
+      mapInstance.on("load", () => {
         const bounds = new mapboxgl.LngLatBounds();
 
         lotes.forEach((lote) => {
           if (lote.coordinates.length > 0) {
             // Agregar fuente y capa para el polígono
             mapInstance.addSource(`lote-${lote.id}`, {
-              type: 'geojson',
+              type: "geojson",
               data: {
-                type: 'Feature',
+                type: "Feature",
                 geometry: {
-                  type: 'Polygon',
+                  type: "Polygon",
                   coordinates: lote.coordinates,
                 },
-                properties: { ...lote },
+                properties: {
+                  ...lote,
+                  len: lote.len || 0, // Proporciona un valor predeterminado si `len` no está definido
+                  reflen: lote.reflen || 0, // Proporciona un valor predeterminado si `reflen` no está definido
+                  class: lote.class || "default", // Proporciona un valor predeterminado si `class` no está definido
+                },
               },
             });
 
             mapInstance.addLayer({
               id: `lote-fill-${lote.id}`,
-              type: 'fill',
+              type: "fill",
               source: `lote-${lote.id}`,
               layout: {},
               paint: {
-                'fill-color': '#088',
-                'fill-opacity': 0.1,
+                "fill-color": "#088",
+                "fill-opacity": 0.1,
               },
             });
 
             mapInstance.addLayer({
               id: `lote-line-${lote.id}`,
-              type: 'line',
+              type: "line",
               source: `lote-${lote.id}`,
               layout: {},
               paint: {
-                'line-color': '#FFF',
-                'line-width': 1,
+                "line-color": "#FFF",
+                "line-width": 1,
               },
             });
 
-            // Calcular el centro del polígono
-            const center = lote.coordinates[0].reduce(
-              (acc, coord) => [acc[0] + coord[0], acc[1] + coord[1]],
-              [0, 0]
-            ).map((val) => val / lote.coordinates[0].length);
+            mapInstance.on("error", (e) => {
+              console.error("Mapbox error:", e.error);
+            });
+
+            // Calcular el centroide del polígono
+            const center = calculateCentroid(lote.coordinates);
 
             // Crear un marcador personalizado con el ID del lote sin el prefijo "lote_"
-            const markerElement = document.createElement('div');
-            markerElement.style.fontSize = '14px';
-            markerElement.style.fontWeight = 'bold';
-            markerElement.style.color = 'white';
-            markerElement.style.textShadow = 'none'; // Sin sombra
-            markerElement.style.border = '1px solid white'; // Borde blanco
-            markerElement.style.borderRadius = '50%'; // Forma circular
-            markerElement.style.padding = '5px';
-            markerElement.style.textAlign = 'center';
-            markerElement.style.width = '30px';
-            markerElement.style.height = '30px';
-            markerElement.style.display = 'flex';
-            markerElement.style.alignItems = 'center';
-            markerElement.style.justifyContent = 'center';
-            markerElement.style.backgroundColor = 'transparent'; // Sin fondo
-            markerElement.textContent = lote.id.replace('lote_', ''); // Eliminar el prefijo "lote_"
+            const markerElement = document.createElement("div");
+            markerElement.style.fontSize = "14px";
+            markerElement.style.fontWeight = "bold";
+            markerElement.style.color = "white";
+            markerElement.style.textShadow = "none"; // Sin sombra
+            markerElement.style.border = "1px solid white"; // Borde blanco
+            markerElement.style.borderRadius = "50%"; // Forma circular
+            markerElement.style.padding = "5px";
+            markerElement.style.textAlign = "center";
+            markerElement.style.width = "30px";
+            markerElement.style.height = "30px";
+            markerElement.style.display = "flex";
+            markerElement.style.alignItems = "center";
+            markerElement.style.justifyContent = "center";
+            markerElement.style.backgroundColor = "transparent"; // Sin fondo
+            markerElement.textContent = lote.id.replace("lote_", ""); // Eliminar el prefijo "lote_"
 
             const marker = new mapboxgl.Marker({ element: markerElement })
               .setLngLat(center)
@@ -139,25 +179,71 @@ export const Map = () => {
             });
 
             // Detectar clics en el polígono
-            mapInstance.on('click', `lote-fill-${lote.id}`, () => {
+            mapInstance.on("click", `lote-fill-${lote.id}`, () => {
               setSelectedLote(lote);
-              mapInstance.setPaintProperty(`lote-fill-${lote.id}`, 'fill-opacity', 0.5); // Cambiar opacidad
+
+              // Calcular el desplazamiento para que el polígono quede a la derecha
+              const offset = [
+                window.innerWidth / 4, // Desplazar hacia la derecha (1/4 del ancho de la ventana)
+                0, // Sin desplazamiento vertical
+              ];
+
+              mapInstance.setPaintProperty(
+                `lote-fill-${lote.id}`,
+                "fill-opacity",
+                0.5
+              ); // Cambiar opacidad
               mapInstance.flyTo({
-                center: center,
-                zoom: 15, // Zoom hacia el polígono
-                speed: 1.5,
+                center: center, // Centrar en el centroide del polígono
+                zoom: 15, // Nivel de zoom
+                speed: 1.5, // Velocidad del vuelo
+                offset, // Aplicar el desplazamiento
               });
             });
 
             // Detectar clics en el marcador
-            marker.getElement().addEventListener('click', () => {
+            marker.getElement().addEventListener("click", () => {
               setSelectedLote(lote);
-              mapInstance.setPaintProperty(`lote-fill-${lote.id}`, 'fill-opacity', 0.5); // Cambiar opacidad
+
+              // Calcular el desplazamiento para que el polígono quede a la derecha
+              const offset = [
+                window.innerWidth / 4, // Desplazar hacia la derecha (1/4 del ancho de la ventana)
+                0, // Sin desplazamiento vertical
+              ];
+
+              mapInstance.setPaintProperty(
+                `lote-fill-${lote.id}`,
+                "fill-opacity",
+                0.5
+              ); // Cambiar opacidad
               mapInstance.flyTo({
-                center: center,
-                zoom: 15, // Zoom hacia el marcador
-                speed: 1.5,
+                center: center, // Centrar en el centroide del polígono
+                zoom: 15, // Nivel de zoom
+                speed: 1.5, // Velocidad del vuelo
+                offset, // Aplicar el desplazamiento
               });
+            });
+            // Cambiar dinámicamente el tamaño del marcador según el nivel de zoom
+            mapInstance.on("zoom", () => {
+              const zoom = mapInstance.getZoom();
+              if (zoom < 13) {
+                const size = Math.max(10, Math.min(20, zoom * 2)); // Ajustar el tamaño entre 10px y 30px
+                markerElement.style.width = `${size}px`;
+                markerElement.style.height = `${size}px`;
+                markerElement.style.padding = `${size / 6}px`;
+                markerElement.style.fontSize = `${14 * 0.75}px`; // 3/4 de 14px
+              } else {
+                markerElement.style.width = "30px";
+                markerElement.style.height = "30px";
+                markerElement.style.padding = "5px";
+              }
+              if (zoom < 12) {
+                // Ocultar el marcador
+                markerElement.style.display = "none";
+              } else {
+                // Mostrar el marcador
+                markerElement.style.display = "flex";
+              }
             });
           }
         });
@@ -180,41 +266,54 @@ export const Map = () => {
   }, [lotes]);
 
   return (
-    <Box sx={{ position: 'relative', width: '100%', height: '100%', backgroundColor: 'gray' }}>
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "gray",
+      }}
+    >
       {loading && (
         <Box
           sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
             zIndex: 1000,
           }}
         >
           <CircularProgress />
         </Box>
       )}
-      <Box sx={{ width: '100%', height: '100%' }} id="map"></Box>
+      <Box sx={{ width: "100%", height: "100%" }} id="map"></Box>
 
       {/* Barra lateral para rotación */}
       <Box
         sx={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
+          position: "absolute",
+          top: "10px",
+          right: "10px",
           zIndex: 1001,
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '10px',
-          background: 'rgba(0, 0, 0, 0.5)',
-          borderRadius: '8px',
-          padding: '10px',
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          background: "rgba(0, 0, 0, 0.5)",
+          borderRadius: "8px",
+          padding: "10px",
         }}
       >
-        <IconButton onClick={() => map.setBearing(map.getBearing() - 10)} sx={{ color: 'white' }}>
+        <IconButton
+          onClick={() => map.setBearing(map.getBearing() - 10)}
+          sx={{ color: "white" }}
+        >
           <RotateLeft />
         </IconButton>
-        <IconButton onClick={() => map.setBearing(map.getBearing() + 10)} sx={{ color: 'white' }}>
+        <IconButton
+          onClick={() => map.setBearing(map.getBearing() + 10)}
+          sx={{ color: "white" }}
+        >
           <RotateRight />
         </IconButton>
       </Box>
@@ -222,34 +321,40 @@ export const Map = () => {
       {/* Barra de controles en la parte inferior */}
       <Box
         sx={{
-          position: 'absolute',
-          bottom: '5px',
-          left: '50%',
-          transform: 'translateX(-50%)',
+          position: "absolute",
+          bottom: "5px",
+          left: "50%",
+          transform: "translateX(-50%)",
           zIndex: 1001,
-          display: 'flex',
-          gap: '10px',
-          background: 'rgba(0, 0, 0, 0.5)',
-          borderRadius: '8px',
-          padding: '10px',
+          display: "flex",
+          gap: "10px",
+          background: "rgba(0, 0, 0, 0.5)",
+          borderRadius: "8px",
+          padding: "10px",
         }}
       >
-        <IconButton onClick={() => map.panBy([-100, 0])} sx={{ color: 'white' }}>
+        <IconButton
+          onClick={() => map.panBy([-100, 0])}
+          sx={{ color: "white" }}
+        >
           <ArrowBack />
         </IconButton>
-        <IconButton onClick={() => map.panBy([0, -100])} sx={{ color: 'white' }}>
+        <IconButton
+          onClick={() => map.panBy([0, -100])}
+          sx={{ color: "white" }}
+        >
           <ArrowUpward />
         </IconButton>
-        <IconButton onClick={() => map.panBy([0, 100])} sx={{ color: 'white' }}>
+        <IconButton onClick={() => map.panBy([0, 100])} sx={{ color: "white" }}>
           <ArrowDownward />
         </IconButton>
-        <IconButton onClick={() => map.panBy([100, 0])} sx={{ color: 'white' }}>
+        <IconButton onClick={() => map.panBy([100, 0])} sx={{ color: "white" }}>
           <ArrowForward />
         </IconButton>
-        <IconButton onClick={() => map.zoomIn()} sx={{ color: 'white' }}>
+        <IconButton onClick={() => map.zoomIn()} sx={{ color: "white" }}>
           <Add />
         </IconButton>
-        <IconButton onClick={() => map.zoomOut()} sx={{ color: 'white' }}>
+        <IconButton onClick={() => map.zoomOut()} sx={{ color: "white" }}>
           <Remove />
         </IconButton>
         <IconButton
@@ -262,7 +367,7 @@ export const Map = () => {
               map.fitBounds(bounds, { padding: 50, duration: 2000 });
             }
           }}
-          sx={{ color: 'white' }}
+          sx={{ color: "white" }}
         >
           <HomeIcon />
         </IconButton>
@@ -271,30 +376,37 @@ export const Map = () => {
       {selectedLote && (
         <Card
           style={{
-            position: 'absolute',
-            bottom: '50%',
-            left: '20px',
+            position: "absolute",
+            bottom: "50%",
+            left: "20px",
             zIndex: 1000,
-            width: '300px',
+            width: "300px",
           }}
         >
           <CardContent>
-            <Typography variant="h6">{`Lote ${selectedLote.id.replace('lote_', '')}`}</Typography>
+            <Typography variant="h6">{`Lote ${selectedLote.id.replace(
+              "lote_",
+              ""
+            )}`}</Typography>
             <Typography variant="body2">
-              <strong>Estado:</strong> {selectedLote.estado || 'N/A'}
+              <strong>Estado:</strong> {selectedLote.estado || "N/A"}
             </Typography>
             <Typography variant="body2">
-              <strong>Superficie:</strong> {selectedLote.superficie || 'N/A'} m²
+              <strong>Superficie:</strong> {selectedLote.superficie || "N/A"} Ha.
             </Typography>
             <Typography variant="body2">
-              <strong>Valor:</strong> ${selectedLote.valor || 'N/A'}
+              <strong>Valor:</strong> ${selectedLote.valor || "N/A"}
             </Typography>
             <Button
               variant="contained"
               color="secondary"
-              style={{ marginTop: '10px' }}
+              style={{ marginTop: "10px" }}
               onClick={() => {
-                map.setPaintProperty(`lote-fill-${selectedLote.id}`, 'fill-opacity', 0.1); // Restaurar opacidad
+                map.setPaintProperty(
+                  `lote-fill-${selectedLote.id}`,
+                  "fill-opacity",
+                  0.1
+                ); // Restaurar opacidad
                 setSelectedLote(null);
                 const bounds = new mapboxgl.LngLatBounds();
                 lotes.forEach((lote) => {
